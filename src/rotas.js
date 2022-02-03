a1f07d087e4632b18359b939be31a69f8eba5787
Robert <robertbezerrads@gmail.com>
2022-02-03 19:52:54 -0300

    feature: estilizando hearder

--- src/rotas.js
+++ src/rotas.js
@@ -1,14 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './pages/Home'
+
+import Header from "./components/Header";
+
+import Home from "./pages/Home";
 
 const Rotas = () => {
-  return(
+  return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
 export default Rotas;
